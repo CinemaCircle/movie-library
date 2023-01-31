f4e08e57ca313c3a5df95221db32237b006073d1
Hamid Farmani <hamidfarmani1@gmail.com>
2023-01-31 19:58:45 +0100

    feat: add Google login
    - Integrate Google Sign-In API into the application
    - Add a "Login with Google" button to the login page
    - Implement a flow for handling the authentication process using Google credentials

--- components/Login/LoginWithGoogle.js
+++ components/Login/LoginWithGoogle.js
@@ -1,8 +1,58 @@
-import { Button } from '@mantine/core';
-import { BrandGoogle } from 'tabler-icons-react';
+import { Avatar, Button, Group, Text, UnstyledButton } from "@mantine/core";
+import { signIn, useSession } from "next-auth/react";
+import { BrandGoogle } from "tabler-icons-react";
 
-export default function LoginWithGoogle(){
-    return <Button component="a" href="#" variant="outline" leftIcon={<BrandGoogle size={14} />} color='red'>
-                Login
-           </Button>
+export default function LoginWithGoogle() {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      {session && session.user ? (
+        <Group position="center">
+          <UnstyledButton
+            sx={(theme) => ({
+              display: "block",
+              width: "100%",
+              padding: theme.spacing.md,
+              color:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[0]
+                  : theme.black,
+
+              "&:hover": {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[8]
+                    : theme.colors.gray[0],
+              },
+            })}
+          >
+            <Group>
+              <Avatar radius="xl" src={session.user.image} alt="it's me" />
+
+              <div style={{ flex: 1 }}>
+                <Text size="sm" weight={500}>
+                  {session.user.name}
+                </Text>
+                <Text color="dimmed" size="xs">
+                  {session.user.email}
+                </Text>
+              </div>
+            </Group>
+          </UnstyledButton>
+        </Group>
+      ) : (
+        <Button
+          component="a"
+          href="#"
+          variant="outline"
+          leftIcon={<BrandGoogle size={14} />}
+          color="red"
+          onClick={() => signIn("google")}
+        >
+          Login
+        </Button>
+      )}
+    </>
+  );
 }
