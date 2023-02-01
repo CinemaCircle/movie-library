82daccc6d6da80b34fe9dc5478a4847b441ff275
Hamid Farmani <hamidfarmani1@gmail.com>
2023-02-01 14:20:30 +0100

    fix: use the backend of Next

---
+++ pages/api/movies/index.js
@@ -0,0 +1,30 @@
+import { addMovie, getMovies } from "@/lib/mongo/movies";
+
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      const { movies, error } = await getMovies();
+
+      if (error) throw new Error(error);
+
+      return res.status(200).json({ movies });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const { id, error } = await addMovie(req.body);
+
+      if (error) throw new Error(error);
+
+      return res.status(200).json({ id });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  res.setHeader("Allow", ["GET", "POST"]);
+  res.status(405).end(`Method ${req.method} is not allowed`);
+};
+
+export default handler;
--- server/routes/getRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getMovie, setMovie } = require("../controllers/movieController");
-
-router.get("/movies", getMovie);
-router.post("/movies", setMovie);
-
-module.exports = router;
