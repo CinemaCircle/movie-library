421c78870c7695c09042f7be4ce04835d2566cea
Hamid Farmani <hamidfarmani1@gmail.com>
2023-02-01 11:03:05 +0100

    feat: create controller and error handler
    - Create a new controller to handle incoming requests and generate responses
    - Implement error handling to catch and respond to errors that may occur during the processing of requests

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,10 +1,16 @@
-const getMovie = (req, res) => {
-  res.status(200).json({ message: "Getting movie" });
-};
+const asyncHandler = require("express-async-handler");
 
-const setMovie = (req, res) => {
+const getMovie = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Getting movie" });
+});
+
+const setMovie = asyncHandler(async (req, res) => {
+  if (!req.body.q) {
+    res.status(400);
+    throw new Error("Please enter the query");
+  }
   res.status(200).json({ message: "Setting movie" });
-};
+});
 
 module.exports = {
   getMovie,
